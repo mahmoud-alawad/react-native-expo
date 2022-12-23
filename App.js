9c1e9175ae18463a16c5536d6504d95b0045cd8e
Mahmoud Alawad <76591349+mahmoud-alawad@users.noreply.github.com>
2022-12-23 22:28:21 +0100

    Removed unnecessary navigation

--- App.js
+++ App.js
@@ -7,38 +7,6 @@ import { BottomTabScreen } from './navigations';
 import { Ionicons } from '@expo/vector-icons';
 import Profile from './screens/Profile';
 
-const Stack = createNativeStackNavigator();
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-     <Tab.Navigator
-     screenOptions={({ route }) => ({
-       tabBarIcon: ({ focused, color, size }) => {
-         let iconName;
-
-         if (route.name === 'Home') {
-           iconName = focused
-             ? 'ios-information-circle'
-             : 'ios-information-circle-outline';
-         } else if (route.name === 'Settings') {
-           iconName = focused ? 'ios-list' : 'ios-list-outline';
-         }
-
-         // You can return any component that you like here!
-         return <Ionicons name={iconName} size={size} color={color} />;
-       },
-       tabBarActiveTintColor: 'tomato',
-       tabBarInactiveTintColor: 'gray',
-     })}
-   >
-     <Tab.Screen name="Home" component={Home} />
-     <Tab.Screen name="Settings" component={Profile} />
-   </Tab.Navigator>
-  );
-}
-
 function App() {
   return (
     <NavigationContainer>
